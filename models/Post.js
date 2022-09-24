09fb250c464352420a52c0434854746042ae1319
Tara Evans <tarajevans@hotmail.com>
2022-09-23 20:32:15 -0400

    creating models

---
+++ models/Post.js
@@ -0,0 +1,42 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+// create our Post model
+class Post extends Model {}
+// create fields/columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    content_txt: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: Date.now,
+      },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "posts",
+  }
+);
+
+module.exports = Post;
